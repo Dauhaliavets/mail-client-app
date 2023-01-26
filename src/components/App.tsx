2acb283d4170fb562d3c5f70aef70095ac97af99
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2023-01-26 03:14:21 +0300

    feat: implement routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,24 @@
+import Container from 'react-bootstrap/esm/Container'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { io } from 'socket.io-client'
+import { BASE_URL } from '../constants/baseUrl'
+import { Home } from './home/Home'
+import { SignIn } from './signIn/SignIn'
+
+const socket = io(BASE_URL)
+
 function App() {
-  return <div>{'Some text'}</div>
+  return (
+    <BrowserRouter>
+      <h2 className='p-3 d-flex justify-content-center'>Anonymous Mail Client</h2>
+      <Container className='p-3 d-flex justify-content-center'>
+        <Routes>
+          <Route path='/' element={<SignIn socket={socket} />}></Route>
+          <Route path='/home' element={<Home socket={socket} />}></Route>
+        </Routes>
+      </Container>
+    </BrowserRouter>
+  )
 }
 
 export default App
